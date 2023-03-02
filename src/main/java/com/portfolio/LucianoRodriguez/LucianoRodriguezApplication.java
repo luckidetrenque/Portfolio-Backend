0f263eea2b50ef7351105443cb5cf64dad9a0890
Luciano Rodriguez <luckidetrenque@gmail.com>
2023-03-02 12:57:38 -0300

    Starting the API Rest in Spring Boot

---
+++ src/main/java/com/portfolio/LucianoRodriguez/LucianoRodriguezApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.LucianoRodriguez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucianoRodriguezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucianoRodriguezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/LucianoRodriguez/LucianoRodriguezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.LucianoRodriguez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucianoRodriguezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
